af1cc2574a434661c67a3f51f12fc8b300fc9538
Edson <edson_csl@hotmail.com>
2020-03-31 17:25:26 -0300

    add new views, and use function fs, path

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path')
 module.exports = {
   index: (req, res) => {
     const servicos = [
@@ -15,13 +17,75 @@ module.exports = {
   },
   contato : (req, res) => {
     const {nome, email, mensagem} = req.body;
-    console.log(nome, email, mensagem)
+    
+    let infoContato = {nome, email, mensagem};
+    // caminho do arquivo
+    let fileContato = path.join('db', 'contatos.json');
+    let listaContato = []
+    
+    if(fs.existsSync(fileContato)){
+      listaContato = fs.readFileSync(fileContato, {encoding: 'utf-8'});
+      
+      listaContato = JSON.parse(listaContato) // converte pra obj
+
+    } 
+    
+    listaContato.push(infoContato)
+    
+    listaContato = JSON.stringify(listaContato);
+    
+    fs.writeFileSync(fileContato, listaContato);
+
     res.render('contato', {nome, email, mensagem, title: 'contato'})
     
   },
   newsletter : (req, res) => {
     const {nome, email} = req.body;
-    res.render('sucesso', {nome, email, title: 'Newsletter Sucesso'})
+    let infoContato = {nome, email};
+    const date = new Date();
+
+   
+    infoContato.data = `${date.getDate()}:${(date.getMonth() + 1)}:${date.getFullYear()}`
+    infoContato.horario = `${date.getHours()}:${date.getMinutes()}`
+    // caminho do arquivo
+    let fileContato = path.join('db', 'newletter.json');
+    let listaContato = []
     
-  }
+    if(fs.existsSync(fileContato)){
+      listaContato = fs.readFileSync(fileContato, {encoding: 'utf-8'});
+      
+      listaContato = JSON.parse(listaContato) // converte pra obj
+
+    } 
+    
+    listaContato.push(infoContato)
+    
+    listaContato = JSON.stringify(listaContato);
+    
+    fs.writeFileSync(fileContato, listaContato);
+
+    res.render('sucesso', {nome, email, title: 'Newsletter Sucesso'})
+  },
+  listFiles: (req, res) => {
+      let filewNewletter = path.join('db', 'newletter.json');
+      let fileContato = path.join('db', 'contatos.json');
+      let listaContato = []
+      let listaNew = []
+      let lista = []
+      if(fs.existsSync(fileContato)){
+        listaContato = fs.readFileSync(fileContato, {encoding: 'utf-8'});
+        
+      
+
+        listaNew =fs.readFileSync( filewNewletter, {encoding: 'utf-8'});
+        listaContato = JSON.parse(listaContato);
+        listaNew = JSON.parse(listaNew)
+        
+        // converte pra obj
+        console.log(lista)
+      } 
+      
+     
+      res.render('listFiles', { title: 'Ola Sucesso', listaNew, listaContato})
+    }
 }
